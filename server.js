6735e594a2b2fc066188b8e947b47ce8fc33656f
Achim Staebler <achim.staebler@appdynamics.com>
2016-12-08 00:02:34 +0100

    instrument node app

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
+require("appdynamics").profile({
+  controllerHostName: '5.189.161.196',
+  controllerPort: 2080,
+  controllerSslEnabled: false,  // Set to true if controllerPort is SSL
+  accountName: 'customer1',
+  accountAccessKey: 'H16h53cur3',
+  applicationName: 'YOUR-APP-NAME',
+  tierName: 'frontend',
+  nodeName: 'process' // The controller will automatically append the node name with a unique number
+});
+
 var express = require('express'),
     employees = require('./routes/employees'),
-    app = express();
+    app = express(),
+    http = require('http');
 
 app.use(express.static('www'));
 
@@ -13,6 +25,35 @@ app.all('*', function(req, res, next) {
 
 app.get('/employees', employees.findAll);
 app.get('/employees/:id', employees.findById);
+
+app.get('/realweather', function(req, res) {
+
+    //var location = 'London';
+    var location = req.query.geo;
+    if (!location) {
+      return res.status(404).end();
+    }
+    var weather;
+
+    var httpAddress = 'http://api.openweathermap.org/data/2.5/weather?q=' + location + '&appid=716f8bdc119843c7e88b7ada22d5d7c3';
+    http.request(httpAddress, function(response) {
+
+        //log('HEADERS: ' + JSON.stringify(res.headers));
+        response.setEncoding('utf8');
+
+        response.on('data', function (chunk) {
+            console.log('BODY: ' + chunk);
+            weather = chunk
+        });
+
+        response.on('end', function() {
+            var backbutton = '<button onclick=\"goBack()\">Go Back</button><script>function goBack() {     window.history.back();  }  </script>';
+            res.send(backbutton + '<BR><BR>The weather in ' + location + ' is <BR><BR>' + weather);
+            console.log('Weather done!')
+        });
+
+    }).end();
+})
 
 app.set('port', process.env.PORT || 5000);
 
